92cecdc81551616fec8199d4fcef26e0c797265a
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2023-01-13 12:22:21 -0300

    Salvar

--- app/src/main/java/com/example/recyclerviewelayouts/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewelayouts/MainActivity.kt
@@ -6,19 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //        /* Inicializar Funções(): */
-//        actionBarCenter()
-//        private fun actionBarCenter() {
-//            supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-//            supportActionBar?.setCustomView(R.layout.actionbar_center)
-//        }
 
+        /* Functions: */
+        actionBarCenter()
 
         val items = arrayListOf(
             1 to "Titulo",
@@ -29,18 +26,14 @@ class MainActivity : AppCompatActivity() {
             0 to "Item E"
         )
 
-
         val adapter = MyAdapter(items)
-        findViewById<RecyclerView>(R.id.rcclrVw_mainActvt).adapter = adapter
-
+        findViewById<RecyclerView>(R.id.rcclrVw_mainActvt).adapter = adapter;
     }
 
     class MyAdapter(private val items: List<Pair<Int, String>>) :
         RecyclerView.Adapter<MyAdapter.Holder>() {
 
-
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-
             return if (viewType == 0) {
                 MyContentHolder(
                     LayoutInflater.from(parent.context).inflate(R.layout.item_a, parent, false)
@@ -51,50 +44,40 @@ class MainActivity : AppCompatActivity() {
                     LayoutInflater.from(parent.context).inflate(R.layout.item_b, parent, false)
                 )
             }
-
         }
 
         override fun getItemViewType(position: Int): Int {
-            return items[position].first
+            return items[position].first;
         }
 
         override fun onBindViewHolder(holder: Holder, position: Int) {
-
-            holder.bind(items[position])
-
+            holder.bind(items[position]);
         }
 
         override fun getItemCount(): Int {
-            return items.size
+            return items.size;
         }
 
-
         abstract class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            abstract fun bind(obj: Pair<Int, String>)
+            abstract fun bind(obj: Pair<Int, String>);
         }
 
         class MyContentHolder(itemView: View) : Holder(itemView) {
-            private val textView: TextView = itemView.findViewById(R.id.txtVw_itemA)
+            private val textView: TextView = itemView.findViewById(R.id.txtVw_itemA);
 
             override fun bind(obj: Pair<Int, String>) {
-                textView.text = obj.second
-
-
+                textView.text = obj.second;
             }
-
         }
 
         class MyHeaderHolder(itemView: View) : Holder(itemView) {
             override fun bind(obj: Pair<Int, String>) {
-
             }
-
-            /* Teste de Exclusao*/
-
-//sdad
-            ///sadsadasd
-            //ss
-            //ssss
         }
+    }
+
+    private fun actionBarCenter() {
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM;
+        supportActionBar?.setCustomView(R.layout.actionbar_center);
     }
 }
